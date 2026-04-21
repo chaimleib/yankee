0f1869342dde89bc8d7db932a11643da2256b339
Chaim Halbert <chaim.leib.halbert@gmail.com>
2026-04-20 20:12:07 -0700

    lint: restore max-len 100 rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,25 @@
 import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
+import stylistic from "@stylistic/eslint-plugin";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.node },
+  },
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  {
+    plugins: {
+      "@stylistic": stylistic,
+    },
+    rules: {
+      "@stylistic/indent": ["error", 2],
+      "@stylistic/max-len": ["error", { code: 100 }],
+    },
+  },
   {
     rules: {
       "no-unused-vars": ["error", {
